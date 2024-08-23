4487251cd798c40af6f27b2f5bee1ad5962ffa1f
Asif Ali Sahaji <152707064+c0deBurNer-AsiF@users.noreply.github.com>
2024-08-23 23:59:26 +0530

    Create Project_04.py

---
+++ Project_04.py
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MysirG All Batches Page</title>
+    <style>
+        header
+        {
+            text-align:center;
+            
+        }
+        table
+        {
+
+        
+            border: 3px solid rgb(128, 0, 128);
+            text-align:center;
+            margin:auto;
+            width:50%;
+            letter-spacing:1px;
+            border-collapse:collapse;
+            
+            
+            
+            
+
+        }
+        body
+        {
+            background-image:url('background_pic_7.jpeg');
+            
+            background-repeat:no-repeat;
+            background-size:cover;
+            text-transform:capitalize;
+            background-position: center;
+            background-attachment:fixed;
+            width:100%;
+            height:100vh;
+            
+            
+            
+        }
+        q
+        {
+            color:rgb(25, 25, 111);
+        }
+        h1,#h3
+        {
+            color:beige;
+        }
+        th,td
+        {
+            color:bisque;
+
+        }
+        h3
+        {
+            color:aquamarine;
+        }
+        thead
+        {
+            font-size:large;
+            font-weight:bold;
+            
+            
+        }
+        td
+        {
+            color:rgb(100, 200, 128);
+            font-weight:bold;
+        }
+        tr
+        {
+            border: 1px solid skyblue;
+        }
+
+        
+    </style>
+</head>
+<body>
+    <header>
+        <h1>Batches</h1>
+        <h3 id="h3">New Batches</h3>
+        <h3>
+            *Click on<q>Admission open</q> link given in the last column in each row to know about FEE, Syllabus, Days, Duration, prerequisites and other facilities. You can take admission online from this site only.</h3>
+    </header>
+
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>S.No.</th>
+                    <th>course title</th>
+                    <th>start date/time/days</th>
+                    <th>mode of classes</th>
+                    <th>more detail</th>
+                    
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1.</td>
+                    <td>Full stack web development using python Master class</td>
+                    <td>23-Feb-2024|9:00PM|Tue-Fri</td>
+                    <td>Online LIVE</td>
+                    <td><a href="file:///C:/Users/tapan/OneDrive/Documents/HTML_vs%20code/Projects/Project_3/Project_3.html" target="_blank">BATCH DETAILS</a></td>
+                </tr>
+
+                <tr>
+                    <td>2.</td>
+                    <td>Java & DSA</td>
+                    <td>1-Mar-2024|11:00AM|Mon-Wed</td>
+                    <td>Online LIVE</td>
+                    <td><a href="http://saurabhshuklaclasses.com/java-se-dsa-from-beginner-to-pro-in-one-course/">BATCH DETAILS</a></td>
+                </tr>
+
+                <tr>
+                    <td>3.</td>
+                    <td>Ultimate coding C,C++,DSA</td>
+                    <td>15-Apr-2024|7:00PM|Tue-Sat</td>
+                    <td>Recordings of Live classes</td>
+                    <td><a href="http://saurabhshuklaclasses.com/ultimate-coding-foundation-c-c-dsa-and-stl/">BATCH DETAILS</a></td>
+                </tr>
+
+                <tr>
+                    <td>4.</td>
+                    <td>SQL for beginners</td>
+                    <td>21-May-2024|5:00PM|Mon-Sat-Wed</td>
+                    <td>Online|Prerecorded</td>
+                    <td><a href="https://premium.mysirg.com/learn/SQL-Fundamentals--Mastering-the-Language-of-Databases">BATCH DETAILS</a></td>
+                </tr>
+
+                <tr>
+                    <td>5.</td>
+                    <td>Python for beginners</td>
+                    <td>self paced</td>
+                    <td>online LIVE</td>
+                    <td><a href="http://saurabhshuklaclasses.com/python-for-beginners-online-live/">ADMISSION OPEN</a></td>
+                </tr>
+
+                <tr>
+                    <td>6.</td>
+                    <td>advance java</td>
+                    <td>self paced</td>
+                    <td>Recoredings of Live classes</td>
+                    <td><a href="http://saurabhshuklaclasses.com/advance-java/">BATCH DETAILS</a></td>
+                </tr>
+
+                <tr>
+                    <td>7.</td>
+                    <td>STL in C++</td>
+                    <td>self paced</td>
+                    <td>Online LIVE</td>
+                    <td><a href="http://saurabhshuklaclasses.com/stl-in-c/">ADMISSION OPEN</a></td>
+                </tr>
+
+                <tr>
+                    <td>8.</td>
+                    <td>DSA using C++</td>
+                    <td>self paced</td>
+                    <td>Recordings of Live classes</td>
+                    <td><a href="http://saurabhshuklaclasses.com/dsa-using-c/">ADMISSION OPEN</a></td>
+                </tr>
+
+                <tr>
+                    <td>9.</td>
+                    <td>DSA through java</td>
+                    <td>self paced</td>
+                    <td>online LIVE</td>
+                    <td><a href="http://saurabhshuklaclasses.com/data-structures-and-algorithms-using-java-2/">ADMISSION OPEN</a></td>
+                </tr>
+            </tbody>
+
+            <tfoot>
+                <tr>
+                    <td>-</td>
+                    <td>C,C++,DSA,SQL</td>
+                    <td>Nov 2024</td>
+                    <td>Online LIVE</td>
+                    <td>Soon</td>
+                </tr>
+            </tfoot>
+        </table>
+    </main>
+    
+</body>
+</html>
